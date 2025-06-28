1dfeac27b65794726aefef06e483b1efcc385b7b
Brian Cheruiyot <briancheru11@gmail.com>
2025-06-28 20:13:38 +0300

    Add index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(data => renderBooks(data));
+}
+
+function renderBooks(books) {
+  const main = document.querySelector('main');
+  books.forEach(book => {
+    const h2 = document.createElement('h2');
+    h2.textContent = book.name;
+    main.appendChild(h2);
+  });
+
   
 }
 
